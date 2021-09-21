31e5fadff628fbf39df364f7126389825fd1ce87
danisharora099 <danisharora099@gmail.com>
2021-09-21 14:37:40 +0530

    add: contract to generate randomness using the VRFConsumerBase

---
+++ eth/contracts/RequestRandomness.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RandomNumberConsumer is VRFConsumerBase {
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    mapping (bytes32 => uint256) public randomNumber;
+    
+    uint256 public randomNumberResult;
+    
+     constructor() 
+        VRFConsumerBase(
+            0x8C7382F9D8f56b33781fE506E897a4F1e2d17255, // VRF Coordinator
+            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK Token
+        ) 
+    {
+        keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+    
+      /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) virtual internal override {
+        randomNumber[requestId] = randomness;
+        randomNumberResult = randomness;
+    }
+
+}
